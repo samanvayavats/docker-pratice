0dfe4246d5dc13d2cc610d96aea40251fd222428
samanvaya <samayvats@gmail.com>
2025-10-21 22:45:51 +0530

    changes the gitignore

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import { Response , Request } from "express"
+const app = express()
+
+app.get('/' ,(req: Request , res:Response)=>{
+    return res.json({
+        message : "yo yo yo y!!!!!!"
+    })
+})
+
+app.listen(3000 ,()=>{
+    console.log('the app is running on ' ,3000)
+})
