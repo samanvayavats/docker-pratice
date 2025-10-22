c641aed7f94ac82064ba3ed83aeaa59666fa57a7
samanvaya <samanvayavats@gmail.com>
2025-10-22 21:32:48 +0530

    added the docker compose file

--- src/db.ts
+++ src/db.ts
@@ -9,7 +9,7 @@ const User = mongoose.model('User' , userSchema)
 
 const connectDb =async()=>{
     try {
-        mongoose.connect('mongodb://(your container name ):27017/docker-pratice')
+        mongoose.connect( `${process.env.MONGO_URL}/docker-pratice`||'mongodb://localhost:27017/docker-pratice')
         console.log('the db has been conntected succesfully')
     } catch (error) {
         console.log('the error is for connect the db is ',error)
